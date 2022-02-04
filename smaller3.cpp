5da285006a31546c7f9c17b5dc6debcfdcc9e8c0
Alexander C <83742533+Alexander-col@users.noreply.github.com>
2022-02-04 09:34:42 +0000

    task b for lab is working!

---
+++ smaller3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void smaller3 (int a, int b, int c){
+  if (a<b && a<c){
+    std::cout << "The smaller of the three is " << a << std::endl;
+  }else if (b<a && b<c){
+    std::cout << "The smaller of the three is " << b << std::endl;
+  } else if (c<a && c<b){
+    std::cout << "The smaller of the three is"  << c << std::endl;
+  }
+}
+int main(){
+
+  int a;
+  int b;
+  int c;
+  std::cout << "Enter the first number:" << std::endl;
+  std::cin >> a;
+  std::cout << "Enter the second number:" << std::endl;
+  std::cin >> b;
+  std::cout << "Enter the thrid Number:" << std:: endl;
+  std::cin >> c;
+  smaller3(a,b,c);
+}
