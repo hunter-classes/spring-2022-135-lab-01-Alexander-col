4bded83001166789e142db6443758fa0a940273d
Alexander C <83742533+Alexander-col@users.noreply.github.com>
2022-02-04 11:53:17 +0000

    program without leap years into account working!

--- month.cpp
+++ month.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
-
+void month(int year, int mes){
+  std::cout << "among us " << std::endl;
+  if ((mes%2) == 0){
+    std::cout << "30 days" << std::endl;
+  }if ((mes%2) != 0) {
+    std::cout << "31 days" << std::endl;
+  }
+}
 
 int main(){
   int year;
-  int month;
+  int mes;
   std::cout << "Enter year:" << std::endl;
   std::cin >> year;
   std::cout << "Enter month:" << std::endl;
-  std::cin >> month;
+  std::cin >> mes;
+  month(year,mes);
   return 0;
 }
