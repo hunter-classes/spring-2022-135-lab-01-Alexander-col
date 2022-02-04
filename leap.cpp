e86b9a27c2ee32d127b48bae5f037f5973f1449e
Alexander C <83742533+Alexander-col@users.noreply.github.com>
2022-02-04 10:56:20 +0000

    Task C is  fully working

--- leap.cpp
+++ leap.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
 void leap(int year){ 
-  std::cout << year%400 << std::endl;
-  std::cout << year%4 << std::endl;
-  if ((year%400) > 1) {
-    std::cout << "Common year" << std::endl;
-  }else if ((year%4) == 0){
-    std::cout << "Leap year" << std::endl;
-  }else{
-    std::cout << "Common year" << std::endl;
+  // std::cout << year%400 << std::endl;
+  // std::cout << year%4 << std::endl;
+  if ((year%4)!=0){
+    std::cout << "Common Year" << std::endl;
+  } else if ((year%100)!= 0){
+    std::cout << "Leap Year" << std::endl;
+  }else if((year%400)!=0){
+    std::cout << "Common Year";
+  }else {
+    std::cout << "Leap Year" << std::endl;
   }
-  
 }
 
 int main(){
